83de04695b51c57b5d0efd997c260c223fa6e650
Fredrik Schultz <fschultz@users.noreply.github.com>
2012-05-06 11:55:33 +0000

    Initial version of KalikoSearch integration.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS16]$/KalikoCMS/Main/Source;C13827

---
+++ KalikoCMS.Search/KalikoSearchProvider.cs
@@ -0,0 +1,65 @@
+﻿
+namespace KalikoCMS.Search {
+    using System;
+    using KalikoCMS.Events;
+    using KalikoSearch.Core;
+
+    public class KalikoSearchProvider : BaseSearchProvider {
+        private Collection _collection;
+        private static string[] _searchFields = new[] { "title", "summary", "content" };
+
+        public KalikoSearchProvider() {
+            _collection = new Collection("KalikCMS");
+        }
+
+        public override void AddToIndex(IndexItem item) {
+            string key = GetKey(item.PageId, item.LanguageId);
+            PageDocument pageDocument = new PageDocument(key, item);
+            _collection.AddDocument(pageDocument);
+            
+            DoOptimizations();
+        }
+
+        public override void RemoveFromIndex(Guid pageId, int languageId) {
+            string key = GetKey(pageId, languageId);
+            _collection.RemoveDocument(key);
+        }
+
+        public override void DoOptimizations() {
+            _collection.OptimizeIndex();
+        }
+
+        private string GetKey(Guid pageId, int languageId) {
+            string key = string.Format("{0}:{1}", pageId, languageId);
+            return key;
+        }
+
+        public override void Init() {
+            PageFactory.PageSaved += OnPageSaved;
+        }
+
+        void OnPageSaved(object sender, PageEventArgs e) {
+            IndexPage(e.Page);
+        }
+
+        public override SearchResult Search(SearchQuery query) {
+            KalikoSearch.Core.SearchResult searchResult = _collection.Search(query.SearchString, _searchFields, query.MetaData, query.ReturnFromPosition, query.NumberOfHitsToReturn);
+
+            SearchResult result = ConvertResult(searchResult);
+
+            return result;
+        }
+
+        private SearchResult ConvertResult(KalikoSearch.Core.SearchResult searchResult) {
+            SearchResult result = new SearchResult();
+            result.NumberOfHits = searchResult.NumberOfHits;
+            result.SecondsTaken = searchResult.SecondsTaken;
+
+            foreach (var hit in searchResult.Hits) {
+                result.Hits.Add(new SearchHit() { Excerpt = hit.Excerpt, Path = hit.Path, Title = hit.Title, MetaData = hit.MetaData });
+            }
+
+            return result;
+        }
+    }
+}
---
+++ KalikoCMS.Search/PageDocument.cs
@@ -0,0 +1,114 @@
+﻿namespace KalikoCMS.Search {
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using KalikoSearch.Core;
+
+    public class PageDocument : IndexDocument {
+        public PageDocument(string key, IndexItem item) {
+            Key = key;
+            Category = item.Category;
+            Content = item.Content;
+            Created = item.Created;
+            LanguageId = item.LanguageId;
+            MetaData = item.MetaData;
+            Modified = item.Modified;
+            PageId = item.PageId;
+            Path = item.Path;
+            Summary = item.Summary;
+            Title = item.Title;
+            PublishStart = item.PublishStart ?? DateTime.MaxValue;
+            PublishStop = item.PublishStop ?? DateTime.MaxValue;
+        }
+
+        protected DateTime PublishStop {
+            get {
+                return ConvertLongToDate(GetNumericFieldValue("publishStop"));
+            }
+            set {
+                AddNumericField("publishStop", ConvertDateToLong(value));
+            }
+        }
+
+        protected DateTime PublishStart {
+            get {
+                return ConvertLongToDate(GetNumericFieldValue("publishStart"));
+            }
+            set {
+                AddNumericField("publishStart", ConvertDateToLong(value));
+            }
+        }
+
+        protected string Summary {
+            get {
+                return GetFieldValue("summary");
+            }
+            set {
+                AddField("summary", value, FieldStore.Store, FieldIndex.Analyzed);
+            }
+        }
+
+        protected Guid PageId {
+            get {
+                return new Guid(GetFieldValue("pageId"));
+            }
+            set {
+                AddField("pageId", value.ToString(), FieldStore.Store, FieldIndex.DontIndex);
+            }
+        }
+
+        protected DateTime Modified {
+            get {
+                return ConvertStringToDate(GetFieldValue("modified"));
+            }
+            set {
+                AddField("modified", ConvertDateToString(value), FieldStore.Store, FieldIndex.DontIndex);
+            }
+        }
+
+        // TODO: Replace dictionary with list containing store and index flags
+        protected Dictionary<string, string> MetaData {
+            set {
+                foreach (var keyValuePair in value) {
+                    AddField(keyValuePair.Key, keyValuePair.Value, FieldStore.Store, FieldIndex.Analyzed);
+                }
+            }
+        }
+
+        protected int LanguageId {
+            get {
+                return int.Parse(GetFieldValue("languageId"));
+            }
+            set {
+                AddField("languageId", value.ToString(CultureInfo.InvariantCulture), FieldStore.Store, FieldIndex.DontIndex);
+            }
+        }
+
+        protected DateTime Created {
+            get {
+                return ConvertStringToDate(GetFieldValue("created"));
+            }
+            set {
+                AddField("created", ConvertDateToString(value), FieldStore.Store, FieldIndex.DontIndex);
+            }
+        }
+
+        protected string Content {
+            get {
+                return GetFieldValue("content");
+            }
+            set {
+                AddField("content", value, FieldStore.Store, FieldIndex.Analyzed);
+            }
+        }
+
+        protected string Category {
+            get {
+                return GetFieldValue("category");
+            }
+            set {
+                AddField("category", value, FieldStore.Store, FieldIndex.IndexOnly);
+            }
+        }
+    }
+}
