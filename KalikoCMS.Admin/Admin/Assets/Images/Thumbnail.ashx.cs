1343af9c7dd414924aacc68987beb21fa053f737
Fredrik Schultz <fschultz@users.noreply.github.com>
2013-09-30 18:55:12 +0000

    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS16]$/KalikoCMS/Main/Source;C25497

--- KalikoCMS.Admin/Admin/Assets/Images/Thumbnail.ashx.cs
+++ KalikoCMS.Admin/Admin/Assets/Images/Thumbnail.ashx.cs
@@ -15,33 +15,75 @@
 #endregion
 
 namespace KalikoCMS.Admin.Assets.Images {
+    using System;
     using System.IO;
     using System.Web;
+    using Configuration;
+    using Kaliko;
     using Kaliko.ImageLibrary;
 
     public class Thumbnail : IHttpHandler {
+        private HttpContext _context;
 
         public void ProcessRequest(HttpContext context) {
-            string path = context.Server.MapPath(context.Request.QueryString["path"]);
+            string redirectPath;
 
-            var file = new FileInfo(path);
-            if(file.Exists) {
-                context.Response.ContentType = "image/jpg";
+            _context = context;
 
-                var image = new KalikoImage(path);
-                var thumbnailImage = image.GetThumbnailImage(100, 100, ThumbnailMethod.Fit);
-
-                thumbnailImage.StreamJpg(90, file.Name);
+            try {
+                redirectPath = GetThumbnail();
             }
-            else {
-                context.Response.Write("Error");
+            catch (Exception exception) {
+                redirectPath = "error-image.jpg";
+                Logger.Write(exception, Logger.Severity.Major);
             }
+
+            context.Response.Redirect(redirectPath);
+        }
+
+        private string GetThumbnail() {
+            var path = _context.Server.MapPath(_context.Request.QueryString["path"]);
+            var fileName = Path.GetFileName(path);
+            var thumbPath = string.Format("{0}_thumbs/{1}", SiteSettings.Instance.ImageCachePath, fileName);
+            var localThumbPath = _context.Server.MapPath(thumbPath);
+
+            if (!File.Exists(path)) {
+                return "error-image.jpg";
+            }
+
+            if (!File.Exists(localThumbPath)) {
+                CreateThumb(path, localThumbPath);
+                return thumbPath;
+            }
+
+            if (File.GetLastWriteTime(path) > File.GetLastWriteTime(localThumbPath)) {
+                File.Delete(localThumbPath);
+                CreateThumb(path, localThumbPath);
+                return thumbPath;
+            }
+
+            return thumbPath;
+        }
+
+        private void CreateThumb(string path, string localThumbPath) {
+            EnsureThumbnailPath();
+
+            var image = new KalikoImage(path);
+            var thumbnail = image.GetThumbnailImage(128, 128, ThumbnailMethod.Crop);
+            thumbnail.SaveJpg(localThumbPath, 85);
+        }
+
+        private void EnsureThumbnailPath() {
+            var thumbFolder = _context.Server.MapPath(string.Format("{0}_thumbs", SiteSettings.Instance.ImageCachePath));
+            if (Directory.Exists(thumbFolder)) {
+                return;
+            }
+
+            Directory.CreateDirectory(thumbFolder);
         }
 
         public bool IsReusable {
-            get {
-                return false;
-            }
+            get { return false; }
         }
     }
 }
