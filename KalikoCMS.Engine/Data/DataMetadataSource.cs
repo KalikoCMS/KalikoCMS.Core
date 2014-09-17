3f7db65510b0c7db9a4886871864d56b2300288d
Fredrik Schultz <fschultz@users.noreply.github.com>
2014-09-17 20:36:35 +0200

    Added missing files in branch

---
+++ KalikoCMS.Engine/Data/DataMetadataSource.cs
@@ -0,0 +1,44 @@
+﻿#region License and copyright notice
+/* 
+ * Kaliko Content Management System
+ * 
+ * Copyright (c) Fredrik Schultz
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ */
+#endregion
+
+namespace KalikoCMS.Data {
+    using Maps;
+    using Telerik.OpenAccess.Metadata.Fluent;
+    using System.Collections.Generic;
+
+    public class DataMetadataSource : FluentMetadataSource {
+        protected override IList<MappingConfiguration> PrepareMapping() {
+            var configurations = new List<MappingConfiguration> {
+                new DataStoreMap(), 
+                new PageInstanceMap(), 
+                new PageMap(),
+                new PagePropertyMap(),
+                new PageTagMap(),
+                new PageTypeMap(),
+                new PropertyMap(),
+                new PropertyTypeMap(),
+                new SiteLanguageMap(),
+                new TagContextMap(),
+                new TagMap()
+            };
+
+            return configurations;
+        }
+    }
+}
