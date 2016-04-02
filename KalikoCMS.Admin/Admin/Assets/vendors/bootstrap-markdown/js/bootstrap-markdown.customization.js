f84fc47f1e2505bb86d279e1582d8143b0c9058c
Fredrik Schultz <fschultz@users.noreply.github.com>
2016-04-02 15:59:14 +0200

    Using CMS dialogs for links and images in Markdown editor #86

---
+++ KalikoCMS.Admin/Admin/Assets/vendors/bootstrap-markdown/js/bootstrap-markdown.customization.js
@@ -0,0 +1,52 @@
+﻿
+
+(function ($) {
+    $.fn.markdown.defaults.buttons[0].forEach(function (group) {
+        group.data.forEach(function (button) {
+            if (button.name === 'cmdUrl') {
+                button.callback = urlCallback;
+            }
+            if (button.name === 'cmdImage') {
+                button.icon = { 'fa-3': 'icon-image' };
+                button.callback = imageCallback;
+            }
+        });
+    });
+
+    function urlCallback(e) {
+        var chunk, cursor, selected = e.getSelection();
+
+        if (selected.length === 0) {
+            chunk = 'link description';
+        } else {
+            chunk = selected.text;
+        }
+
+        top.registerCallback(function(newUrl, newType) {
+            e.replaceSelection('[' + chunk + '](' + newUrl + ')');
+            cursor = selected.start + 1;
+            e.setSelection(cursor, cursor + chunk.length);
+        });
+
+        top.propertyEditor.dialogs.openSelectLinkDialog(chunk, 0);
+    }
+
+    function imageCallback(e) {
+        var chunk, cursor, selected = e.getSelection();
+
+        if (selected.length === 0) {
+            chunk = e.__localize('image description');
+        } else {
+            chunk = selected.text;
+        }
+
+        top.registerCallback(function (imagePath, cropX, cropY, cropW, cropH, originalPath, description) {
+            e.replaceSelection('![' + description + '](' + imagePath + ')');
+            cursor = selected.start + 2;
+            e.setSelection(cursor, cursor + imagePath.length);
+        });
+
+        top.propertyEditor.dialogs.openEditImageDialog('', '', '', '', '', '', '', '', chunk);
+    }
+
+})(jQuery);
